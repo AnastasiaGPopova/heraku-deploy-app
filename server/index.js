f13525c3e959fa585d23d39a8f712c257fd126bb
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-04-04 13:05:18 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(routes)
 
 app.get('*'), function (req,res){
   res.sendFile(
-    path.join(__dirname, '../client/build/index.html'),
+    path.resolve(__dirname, 'client', 'build','index.html'),
     function (err){
       if(err){
         res.status(500).send(err)
