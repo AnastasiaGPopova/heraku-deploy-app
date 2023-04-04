31d855f2894cdf68a09d5aac034c748bd8a30355
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-04-04 13:24:27 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http')
 const mongoose = require('mongoose');
 // const config = require('./configPorts')
 const path = require('path')
