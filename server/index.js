3a03d82350eacaa42aa6577e42976a0442617bc5
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-04-04 14:04:17 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,18 +25,13 @@ app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(authentication());
-app.use(express.static(path.join(__dirname, 'build')));
 // app.use(express.static(buildPath));
 
 app.use(routes)
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/index.html'))
-// });
-
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 
