3e80b65b0b402d384c41ce10365db0e8dc9795ef
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-04-04 12:56:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(authentication());
 app.use(express.static(buildPath))
 app.use(routes)
 
-app.get('/*'), function (req,res){
+app.get('*'), function (req,res){
   res.sendFile(
     path.join(__dirname, '../client/build/index.html'),
     function (err){
